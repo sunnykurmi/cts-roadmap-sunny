1464b621eca81aa070577b2e2618c5607c68b41f
sunny kurmi <sunnykurmiskks@gmail.com>
2024-10-14 17:17:07 +0530

    xsxdsc

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -4,162 +4,169 @@ const User = require("../models/user.schema.js");
 const ErrorHandler = require("../utils/ErrorHandler"); // Fixed typo
 const { sendtoken } = require("../utils/sendtoken");
 const nodemailer = require("nodemailer");
-let path = require('path');
+let path = require("path");
 
-let imagekit = require('../utils/imagekit.js').initImageKit();
+let imagekit = require("../utils/imagekit.js").initImageKit();
 
-// home page tasting 
+// home page tasting
 exports.homepage = catchAsyncErrors(async (req, res, next) => {
-    res.status(200).json({
-        success: true,
-        message: "Welcome to crosstheskylimits"
-    });
+  res.status(200).json({
+    success: true,
+    message: "Welcome to crosstheskylimits",
+  });
 });
 
-// signup student 
+// signup student
 exports.signup = catchAsyncErrors(async (req, res, next) => {
-    const { name, email, contact, password, avatar, date } = req.body;
+  const { name, email, password, avatar, date } = req.body;
 
-    if ([name, email, contact, password].some((field) => field?.trim() === "")) {
-        return next(new ErrorHandler("User details required", 401));
-    }
+  if ([name, email, password].some((field) => field?.trim() === "")) {
+    return next(new ErrorHandler("User details required", 401));
+  }
 
-    const existedUser = await User.findOne({
-        $or: [{ contact }, { email }]
-    });
+  const existedUser = await User.findOne({ email});
 
-    if (existedUser) {
-        return next(new ErrorHandler("User with this email or contact already exists", 409));
-    }
+  if (existedUser) {
+    return next(
+      new ErrorHandler("User with this email or contact already exists", 409)
+    );
+  }
 
-    const user = await User.create({
-        name,
-        email,
-        contact,
-        date,
-        avatar: avatar,
-        password,
-    });
+  const user = await User.create({
+    name,
+    email,
+    date,
+    avatar: avatar,
+    password,
+  });
 
-    sendtoken(user, 200, res);
+  sendtoken(user, 200, res);
 });
 
-// signin student 
+// signin student
 exports.signin = catchAsyncErrors(async (req, res, next) => {
-    let user = await User.findOne({ email: req.body.email }).select("+password").exec();
+  let user = await User.findOne({ email: req.body.email })
+    .select("+password")
+    .exec();
 
-    if (!user) return next(new ErrorHandler("User not found with this email address", 404));
+  if (!user)
+    return next(
+      new ErrorHandler("User not found with this email address", 404)
+    );
 
-    const isMatch = await user.comparepassword(req.body.password);
-    if (!isMatch) return next(new ErrorHandler("Incorrect password", 400));
+  const isMatch = await user.comparepassword(req.body.password);
+  if (!isMatch) return next(new ErrorHandler("Incorrect password", 400));
 
-    sendtoken(user, 200, res);
+  sendtoken(user, 200, res);
 });
 
-// signout student 
+// signout student
 exports.signout = catchAsyncErrors(async (req, res, next) => {
-    res.clearCookie("token");
-    res.json({ message: "Successfully signout!" });
+  res.clearCookie("token");
+  res.json({ message: "Successfully signout!" });
 });
 
 // current student
 exports.currentuser = catchAsyncErrors(async (req, res, next) => {
-    let user = await User.findById(req.id).populate('roadmaps').exec();
+  let user = await User.findById(req.id).populate("roadmaps").exec();
 
-    if (!user) return next(new ErrorHandler("User not found", 404));
+  if (!user) return next(new ErrorHandler("User not found", 404));
 
-    res.json({ success: true, user: user });
+  res.json({ success: true, user: user });
 });
 
 // update student
 exports.edituser = catchAsyncErrors(async (req, res, next) => {
-    let user = await User.findById(req.params.id);
+  let user = await User.findById(req.params.id);
 
-    if (!user) return next(new ErrorHandler("User not found", 404));
+  if (!user) return next(new ErrorHandler("User not found", 404));
 
-    const updates = { ...req.body };
-    delete updates.avatar;
+  const updates = { ...req.body };
+  delete updates.avatar;
 
-    Object.keys(updates).forEach(key => {
-        user[key] = updates[key];
-    });
+  Object.keys(updates).forEach((key) => {
+    user[key] = updates[key];
+  });
 
-    await user.save();
+  await user.save();
 
-    res.json({ success: true, message: "User updated successfully" });
+  res.json({ success: true, message: "User updated successfully" });
 });
 
 // Add education to user
 exports.addEducation = catchAsyncErrors(async (req, res, next) => {
-    const userId = req.params.id;
-    const newEducation = req.body;
+  const userId = req.params.id;
+  const newEducation = req.body;
 
-    const user = await User.findById(userId);
-    if (!user) return next(new ErrorHandler("User not found", 404));
+  const user = await User.findById(userId);
+  if (!user) return next(new ErrorHandler("User not found", 404));
 
-    user.education = newEducation; // Replace the existing education object
-    await user.save();
+  user.education = newEducation; // Replace the existing education object
+  await user.save();
 
-    res.status(200).json({ message: 'Education added successfully', user });
+  res.status(200).json({ message: "Education added successfully", user });
 });
 
 // Add achievement to user
 exports.addAchievement = catchAsyncErrors(async (req, res, next) => {
-    const userId = req.params.id;
-    const newAchievement = req.body.achievement;
+  const userId = req.params.id;
+  const newAchievement = req.body.achievement;
 
-    const user = await User.findById(userId);
-    if (!user) return next(new ErrorHandler("User not found", 404));
+  const user = await User.findById(userId);
+  if (!user) return next(new ErrorHandler("User not found", 404));
 
-    user.achievements.push(newAchievement);
-    await user.save();
+  user.achievements.push(newAchievement);
+  await user.save();
 
-    res.status(200).json({ message: 'Achievement added successfully', user });
+  res.status(200).json({ message: "Achievement added successfully", user });
 });
 
 // Update social media links
 exports.updateSocialMedia = catchAsyncErrors(async (req, res, next) => {
-    const userId = req.params.id;
-    const newSocialMedia = req.body.socialmedia;
+  const userId = req.params.id;
+  const newSocialMedia = req.body.socialmedia;
 
-    const user = await User.findById(userId);
-    if (!user) return next(new ErrorHandler("User not found", 404));
+  const user = await User.findById(userId);
+  if (!user) return next(new ErrorHandler("User not found", 404));
 
-    user.socialmedia = { ...user.socialmedia, ...newSocialMedia };
-    await user.save();
+  user.socialmedia = { ...user.socialmedia, ...newSocialMedia };
+  await user.save();
 
-    res.status(200).json({ message: 'Social media updated successfully', user });
+  res.status(200).json({ message: "Social media updated successfully", user });
 });
 
 exports.resetpassword = catchAsyncErrors(async (req, res, next) => {
-    const user = await User.findById(req.id).exec();
-    user.password = req.body.password;
-    await user.save();
-    sendtoken(user, 201, res);
+  const user = await User.findById(req.id).exec();
+  user.password = req.body.password;
+  await user.save();
+  sendtoken(user, 201, res);
 });
 
 exports.usersendmail = catchAsyncErrors(async (req, res, next) => {
-    const user = await User.findOne({ email: req.body.email }).exec();
+  const user = await User.findOne({ email: req.body.email }).exec();
 
-    if (!user) return next(new ErrorHandler("User not found with this email address", 404));
+  if (!user)
+    return next(
+      new ErrorHandler("User not found with this email address", 404)
+    );
 
-    const url = `${req.protocol}://crosstheskylimits.online/forget-link/${user._id}`;
+  const url = `${req.protocol}://crosstheskylimits.online/forget-link/${user._id}`;
 
-    const transport = nodemailer.createTransport({
-        service: "gmail",
-        host: "smtp.gmail.com",
-        post: 465,
-        auth: {
-            user: process.env.MAIL_EMAIL_ADDRESS,
-            pass: process.env.MAIL_PASSWORD,
-        },
-    });
+  const transport = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    post: 465,
+    auth: {
+      user: process.env.MAIL_EMAIL_ADDRESS,
+      pass: process.env.MAIL_PASSWORD,
+    },
+  });
 
-    const mailOptions = {
-        from: "Cross The SKylimits.",
-        to: req.body.email,
-        subject: "Password Reset Link",
-        html: `
+  const mailOptions = {
+    from: "Cross The SKylimits.",
+    to: req.body.email,
+    subject: "Password Reset Link",
+    html: `
             <div style="text-align: center; font-family: Arial, sans-serif; color: #333;">
                 <h1 style="color: #4CAF50;">Password Reset Request</h1>
                 <p style="margin: 0 0 15px 0;">Click the button below to reset your password.</p>
@@ -170,87 +177,94 @@ exports.usersendmail = catchAsyncErrors(async (req, res, next) => {
                 <p style="margin-top: 20px;">If you did not request a password reset, please ignore this email.</p>
             </div>
         `,
-    };
+  };
 
-    // <img src="https://ik.imagekit.io/3olrnmipz/passresetuser.gif?updatedAt=1727437323262" alt="Password Reset" style="width: 100%; max-width: 600px; height: auto; margin: 20px 0;">
+  // <img src="https://ik.imagekit.io/3olrnmipz/passresetuser.gif?updatedAt=1727437323262" alt="Password Reset" style="width: 100%; max-width: 600px; height: auto; margin: 20px 0;">
 
-    transport.sendMail(mailOptions, (err, info) => {
-        if (err) return next(new ErrorHandler(err, 500));
+  transport.sendMail(mailOptions, (err, info) => {
+    if (err) return next(new ErrorHandler(err, 500));
 
-        return res.status(200).json({
-            message: "mail sent successfully",
-            url,
-        });
+    return res.status(200).json({
+      message: "mail sent successfully",
+      url,
     });
+  });
 
-    user.resetPasswordToken = "1";
-    await user.save();
-    res.json({ user, url });
+  user.resetPasswordToken = "1";
+  await user.save();
+  res.json({ user, url });
 });
 
 exports.userforgetlink = catchAsyncErrors(async (req, res, next) => {
-    const user = await User.findById(req.params.id).exec();
+  const user = await User.findById(req.params.id).exec();
 
-    if (!user) return next(new ErrorHandler("User not found with this email address", 404));
+  if (!user)
+    return next(
+      new ErrorHandler("User not found with this email address", 404)
+    );
 
-    if (user.resetPasswordToken == "1") {
-        user.resetPasswordToken = "0";
-        user.password = req.body.password;
-        await user.save();
-    } else {
-        return next(new ErrorHandler("Invalid Reset Password Link! Please try again", 500));
-    }
-    res.status(200).json({
-        message: "Password has been successfully Changed",
-    });
+  if (user.resetPasswordToken == "1") {
+    user.resetPasswordToken = "0";
+    user.password = req.body.password;
+    await user.save();
+  } else {
+    return next(
+      new ErrorHandler("Invalid Reset Password Link! Please try again", 500)
+    );
+  }
+  res.status(200).json({
+    message: "Password has been successfully Changed",
+  });
 });
 
 exports.studentavatar = catchAsyncErrors(async (req, res, next) => {
-    const student = await User.findById(req.params.id).exec();
-    const file = req.files.avatar;
-    const modifiedFileName = `roadmapbuilder-${Date.now()}${path.extname(file.name)}`;
+  const student = await User.findById(req.params.id).exec();
+  const file = req.files.avatar;
+  const modifiedFileName = `roadmapbuilder-${Date.now()}${path.extname(
+    file.name
+  )}`;
 
-    if (student.avatar.fileId !== "") {
-        await imagekit.deleteFile(student.avatar.fileId);
-    }
+  if (student.avatar.fileId !== "") {
+    await imagekit.deleteFile(student.avatar.fileId);
+  }
 
-    const { fileId, url } = await imagekit.upload({
-        file: file.data,
-        fileName: modifiedFileName,
-        folder: 'profileimgcts',
-    });
-    student.avatar = { fileId, url };
-    await student.save();
-    res.status(200).json({
-        success: true,
-        message: "Profile updated!",
-    });
+  const { fileId, url } = await imagekit.upload({
+    file: file.data,
+    fileName: modifiedFileName,
+    folder: "profileimgcts",
+  });
+  student.avatar = { fileId, url };
+  await student.save();
+  res.status(200).json({
+    success: true,
+    message: "Profile updated!",
+  });
 });
 
 exports.deleteuser = catchAsyncErrors(async (req, res, next) => {
-    const user = await User.findByIdAndDelete(req.params.id).exec();
-    if (!user) return next(new ErrorHandler("User not found", 404));
-    res.status(200).json({
-        success: true,
-        message: "User deleted successfully",
-    });
+  const user = await User.findByIdAndDelete(req.params.id).exec();
+  if (!user) return next(new ErrorHandler("User not found", 404));
+  res.status(200).json({
+    success: true,
+    message: "User deleted successfully",
+  });
 });
 
 // portfolio controllers
 
 // show all portfolio
 exports.showportfolio = catchAsyncErrors(async (req, res, next) => {
-    try {
-        const portfolio = await Portfolio.find()
+  try {
+    const portfolio = await Portfolio.find();
     if (!portfolio) return next(new ErrorHandler("portfolios not found", 404));
     res.status(200).json({
-        success: true,
-        portfolio
+      success: true,
+      portfolio,
     });
-    } catch (error) {
-        res.status(400).json({
-            success: false,
-            error
-        });
-    }
-})
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+});
